86ad920915fe5e2435655ddbe9cdc68c70d90031
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-04-04 12:43:10 -0700

    changed Humanoid and CharacterStats into a class in the prototype-refractor file.

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -14,15 +14,16 @@ Prototype Refactor
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method -> returns the string 'GameObject was removed from the game.' 
 */
-function GameObject(options) {
-    this.createdAt = options.createdAt;
-    this.dimensions = options.dimensions;
-    this.name = options.name;
-  }
-  
-  GameObject.prototype.destroy = function () {
-    return `${this.name} was removed from the game.`;
-  };
+class GameObject {
+    constructor(options){
+        this.createdAt = options.createdAt;
+        this.dimensions = options.dimensions;
+        this.name = options.name;
+    }
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    }
+}
   
   /*
   === CharacterStats ===
@@ -32,17 +33,16 @@ function GameObject(options) {
   * should inherit destroy() from GameObject's prototype
   */
   
-  function CharacterStats(characterStatsOptions) {
-    GameObject.call(this, characterStatsOptions);
-    this.healthPoints = characterStatsOptions.healthPoints;
-  }
-  
-  // Sets up inheritance with GameObject
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  CharacterStats.prototype.takeDamage = function () {
-    return `${this.name} took damage.`;
-  };
+class CharacterStats extends GameObject{
+    constructor(characterStatsOptions){
+        super(characterStatsOptions);
+        this.healthPoints = characterStatsOptions.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
+
   
   /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -53,18 +53,18 @@ function GameObject(options) {
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
   */
-  function Humanoid(humanoidOptions) {
-    CharacterStats.call(this, humanoidOptions);
-    this.team = humanoidOptions.team;
-    this.weapons = humanoidOptions.weapons;
-    this.language = humanoidOptions.language;
-  }
   
-  Humanoid.prototype = Object.create(CharacterStats.prototype);
-  
-  Humanoid.prototype.greet = function () {
-    return `${this.name} offers a greeting in ${this.language}.`;
-  };
+class Humanoid extends CharacterStats {
+    constructor(humanoidOptions){
+        super(humanoidOptions);
+        this.team = humanoidOptions.team;
+        this.weapons = humanoidOptions.weapons;
+        this.language = humanoidOptions.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    }
+}
   
   
   /*
